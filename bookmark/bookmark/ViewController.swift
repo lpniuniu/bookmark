f67420f7d208c035b5feaa2f86f5ad52aa83f9d9
hzfanfei <hzfanfei@corp.netease.com>
2016-12-13 15:08:23 +0800

    add proj

---
+++ bookmark/bookmark/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bookmark
+//
+//  Created by FanFamily on 2016/12/13.
+//  Copyright © 2016年 niuniu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
