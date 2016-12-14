123740c1b00f14895a01cd2213378fc5f644c2e4
fanfei <hzfanfei@corp.netease.com>
2016-12-15 00:08:25 +0800

    slider not work

---
+++ bookmark/bookmark/BookListTableView.swift
@@ -0,0 +1,51 @@
+//
+//  BookListTableView.swift
+//  bookmark
+//
+//  Created by FanFamily on 16/12/14.
+//  Copyright © 2016年 niuniu. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class BookListTableView: UITableView, UITableViewDelegate, UITableViewDataSource {
+    
+    let cellIdentifier:String = "cellIdentifier"
+    
+    override init(frame:CGRect, style: UITableViewStyle) {
+        super.init(frame: frame, style: style)
+        delegate = self
+        dataSource = self
+        separatorStyle = .none
+        register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        showsVerticalScrollIndicator = false
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        cell.configureFlatCell(with: UIColor.clouds(), selectedColor: UIColor.greenSea(), roundingCorners:.allCorners)
+        cell.cornerRadius = 5.0
+        cell.separatorHeight = 20.0
+        cell.backgroundColor = backgroundColor
+        
+        return cell
+    }
+}
--- bookmark/bookmark/BookListViewController.swift
+++ bookmark/bookmark/BookListViewController.swift
@@ -10,57 +10,49 @@ import UIKit
 import FlatUIKit
 import SnapKit
 
-class BookListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class BookListViewController: UIViewController {
     
-    let cellIdentifier:String = "cellIdentifier"
-    let tableView:UITableView = UITableView()
+    
+    let bookListTable:BookListTableView = BookListTableView()
+    let pageSlider:UISlider = UISlider()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         view.backgroundColor = UIColor.lightGray
-        view.addSubview(tableView)
-        tableView.backgroundColor = view.backgroundColor
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.snp.makeConstraints { (maker:ConstraintMaker) in
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: nil, action: nil)
+        installBookListTableView()
+        installPageSlider()
+    }
+    
+    func installBookListTableView() {
+        bookListTable.backgroundColor = view.backgroundColor
+        view.addSubview(bookListTable)
+        bookListTable.snp.makeConstraints { (maker:ConstraintMaker) in
             maker.top.equalTo(view).offset(10)
             maker.left.equalTo(view).offset(10)
             maker.right.equalTo(view).offset(-10)
             maker.bottom.equalTo(view)
         }
-        tableView.separatorStyle = .none
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
-        tableView.showsVerticalScrollIndicator = false
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: nil, action: nil)
+    }
+    
+    func installPageSlider() {
+        bookListTable.addSubview(pageSlider)
+        pageSlider.backgroundColor = UIColor.white
+        let trans = CGAffineTransform(rotationAngle: CGFloat(M_PI)/2.0)
+        pageSlider.transform = trans;
+        pageSlider.snp.makeConstraints { (maker:ConstraintMaker) in
+            maker.centerY.equalTo(bookListTable.subviews[0].snp.centerY)
+            maker.centerX.equalTo(view.snp.right).offset(-15)
+            maker.width.equalTo(657)
+            maker.height.equalTo(30)
+        }
+        pageSlider.configureFlatSlider(withTrackColor: UIColor.silver(), progressColor: UIColor.alizarin(), thumbColor: UIColor.greenSea())
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
-    }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 200
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
-        cell.configureFlatCell(with: UIColor.clouds(), selectedColor: UIColor.greenSea(), roundingCorners:.allCorners)
-        cell.cornerRadius = 5.0
-        cell.separatorHeight = 20.0
-        cell.backgroundColor = view.backgroundColor
-        
-        return cell
     }
 }
 
