1f8ee71511725335208ed17f120ee465d95483b8
hzfanfei <hzfanfei@corp.netease.com>
2017-01-13 17:05:31 +0800

    reload from setting data

--- bookmark/bookmark/BookMeViewController/BookMeViewController.swift
+++ bookmark/bookmark/BookMeViewController/BookMeViewController.swift
@@ -8,37 +8,44 @@
 
 import UIKit
 import Eureka
+import RealmSwift
 
 class BookMeViewController: FormViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let realm = try! Realm()
+        let results = realm.objects(BookSettingData.self);
+        var dict = [String:String]()
+        for result in results {
+            dict[result.key!] = result.value!
+        }
         view.backgroundColor = UIColor.white
         // Do any additional setup after loading the view.
         form = Section("提醒")
             <<< SwitchRow(){
                 $0.title = "鼓励提醒"
-                $0.value = true
+                $0.value = dict["encourage_switch"]! == "1"
             }
             <<< TimeRow(){
                 $0.title = "鼓励提醒时间"
                 let dateFormatter = DateFormatter()
-                let initialString = "21:00:00"
-                dateFormatter.dateFormat = "HH:mm:ss"
+                let initialString = dict["encourage_time"]!
+                dateFormatter.dateFormat = "HH:mm"
                 if let date = dateFormatter.date(from: initialString) {
                     $0.value = date
                 }
             }
             <<< SwitchRow(){
                 $0.title = "督促提醒"
-                $0.value = true
+                $0.value = dict["urge_switch"]! == "1"
             }
             <<< TimeRow(){
                 $0.title = "督促提醒时间"
                 let dateFormatter = DateFormatter()
-                let initialString = "21:00:00"
-                dateFormatter.dateFormat = "HH:mm:ss"
+                let initialString = dict["urge_time"]!
+                dateFormatter.dateFormat = "HH:mm"
                 if let date = dateFormatter.date(from: initialString) {
                     $0.value = date
                 }
