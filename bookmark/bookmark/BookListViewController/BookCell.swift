b4ec0a3b92e5dcaef09d69a248651271557f5799
hzfanfei <hzfanfei@corp.netease.com>
2017-01-18 20:53:44 +0800

    修复swtable的bug

--- bookmark/bookmark/BookListViewController/BookCell.swift
+++ bookmark/bookmark/BookListViewController/BookCell.swift
@@ -22,6 +22,8 @@ class BookCell: SWTableViewCell {
         addSubview(bookImageView)
         addSubview(nameLabel)
         addSubview(pageLabel)
+        
+        build()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -31,20 +33,20 @@ class BookCell: SWTableViewCell {
     override func layoutSubviews() {
         super.layoutSubviews()
         bookImageView.snp.remakeConstraints { (make:ConstraintMaker) in
-            make.left.top.equalTo(self).offset(5)
-            make.bottom.equalTo(self).offset(-25)
-            make.width.equalTo(180)
+            make.left.top.equalToSuperview().offset(5)
+            make.bottom.equalToSuperview().offset(-25)
+            make.width.equalTo(150)
         }
         nameLabel.snp.remakeConstraints { (make:ConstraintMaker) in
-            make.top.equalTo(self).offset(5)
+            make.top.equalToSuperview().offset(5)
             make.left.equalTo(bookImageView.snp.right).offset(5)
-            make.right.equalTo(self).offset(-5)
+            make.right.equalToSuperview().offset(-5)
             make.height.equalTo(35)
         }
         pageLabel.snp.remakeConstraints { (make:ConstraintMaker) in
             make.top.equalTo(nameLabel.snp.bottom).offset(5)
             make.left.equalTo(bookImageView.snp.right).offset(5)
-            make.right.equalTo(self).offset(-5)
+            make.right.equalToSuperview().offset(-5)
             make.height.equalTo(35)
         }
     }
